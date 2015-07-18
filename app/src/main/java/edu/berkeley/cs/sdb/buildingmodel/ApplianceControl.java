b9a6984f8d2d74ae5b2d1b9c83d4fcf8b0868e75
Jack Kolb <jkolb@berkeley.edu>
2015-07-18 15:54:42 -0700

    Added image upload and delay measurement code

--- app/src/main/java/edu/berkeley/cs/sdb/buildingmodel/ApplianceControl.java
+++ app/src/main/java/edu/berkeley/cs/sdb/buildingmodel/ApplianceControl.java
@@ -1,26 +1,41 @@
 package edu.berkeley.cs.sdb.buildingmodel;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.net.Uri;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.util.EntityUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
 public class ApplianceControl extends Activity {
 
     private static final int CAPTURE_IMAGE_REQUEST_CODE = 410;
+    private static final String IMAGE_UPLOAD_URL = "castle.cs.berkeley.edu:32768";
     private Uri mImageUri;
 
     @Override
@@ -37,24 +52,35 @@ public class ApplianceControl extends Activity {
         if (requestCode == CAPTURE_IMAGE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 try {
-                    ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),
-                            mImageUri);
-                    Matrix m = new Matrix();
-                    m.postRotate(90);
-                    Bitmap bmp = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
-                            bitmap.getHeight(), m, true);
-                    imageView.setImageBitmap(bmp);
-                    setInterfaceVisiblity(View.VISIBLE);
-                } catch (IOException e) {
+                    String result = new UploadImageTask().execute().get();
+                    Log.d("ApplianceControl", result);
+                } catch (InterruptedException | ExecutionException e) {
                     throw new RuntimeException(e);
                 }
+                displayImage();
             } else if (resultCode == RESULT_CANCELED) {
                 // If user cancelled operation, just return them to camera app
                 invokeCameraApp();
             } else {
                 throw new RuntimeException("Failed to capture appliance image");
             }
+        }
+    }
+
+    // Display captured image on the screen and show control interface
+    private void displayImage() {
+        try {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),
+                    mImageUri);
+            Matrix m = new Matrix();
+            m.postRotate(90);
+            Bitmap bmp = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
+                    bitmap.getHeight(), m, true);
+            imageView.setImageBitmap(bmp);
+            setInterfaceVisiblity(View.VISIBLE);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -100,5 +126,53 @@ public class ApplianceControl extends Activity {
         dimDownButton.setVisibility(visibility);
         TextView dimText = (TextView) findViewById(R.id.dim_label);
         dimText.setVisibility(visibility);
+    }
+
+    // Uploads captured image to a server and measures the associated delay
+    private class UploadImageTask extends AsyncTask<Void, Void, String> {
+        private ProgressDialog progress;
+        private long startTime;
+        private long endTime;
+
+        @Override
+        protected void onPreExecute() {
+            progress = new ProgressDialog(ApplianceControl.this);
+            progress.setTitle("Loading");
+            progress.setMessage("Uploading image to server...");
+            progress.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            HttpClient client = AndroidHttpClient.newInstance("ApplianceControl");
+            HttpPost httpPost = new HttpPost(IMAGE_UPLOAD_URL);
+            httpPost.setEntity(new FileEntity(new File(mImageUri.toString()), "image/jpeg"));
+
+            try {
+                startTime = System.currentTimeMillis();
+                HttpResponse response = client.execute(httpPost);
+                endTime = System.currentTimeMillis();
+                StatusLine statusLine = response.getStatusLine();
+                if (statusLine.getStatusCode() != HttpStatus.SC_OK) {
+                    String msg = String.format("HTTP Error %d: %s", statusLine.getStatusCode(),
+                            statusLine.getReasonPhrase());
+                    throw new RuntimeException(msg);
+                }
+
+                return EntityUtils.toString(response.getEntity(), "UTF-8");
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                client.getConnectionManager().shutdown();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            progress.dismiss();
+            String msg = String.format("Image posting delay: %dms", endTime - startTime);
+            Toast.makeText(ApplianceControl.this, msg, Toast.LENGTH_LONG).show();
+        }
     }
 }
