deede01be15849769026a42fbba5f5dba17d130a
Kaifei Chen <kaifei@cs.berkeley.edu>
2016-05-08 03:30:57 +0000

    Add Database file to sperate memory and database abstractions

--- server/src/Database.h
+++ server/src/Database.h
@@ -20,31 +20,24 @@ class VWDictionary;
 class VisualWord;
 class Feature2D;
 
-class MemoryLoc
+// the database is read-only for now
+class Database
 {
 public:
     static const int kIdStart;
 
 public:
-    MemoryLoc();
-    virtual ~MemoryLoc();
+    Database();
+    virtual ~Database();
 
-    bool update(const rtabmap::SensorData &data);
     bool init(const std::string &dbUrl,
               const rtabmap::ParametersMap &parameters = rtabmap::ParametersMap());
     void close();
-    std::map<int, float> computeLikelihood(const rtabmap::Signature *signature,
-                                           const std::list<int> &ids);
-
-    void emptyTrash();
-    void deleteLocation(int locationId);
 
     const rtabmap::Signature *getLastWorkingSignature() const;
     rtabmap::Transform getOptimizedPose(int signatureId) const;
     const rtabmap::Signature *getSignature(int id) const;
     const std::map<int, rtabmap::Signature *> &getSignatures() const;
-
-    rtabmap::Transform computeGlobalVisualTransform(int oldId, int newId) const;
 
 private:
     virtual void parseParameters(const rtabmap::ParametersMap &parameters);
@@ -64,20 +57,26 @@ private:
 
 
     void moveToTrash(rtabmap::Signature *s, bool keepLinkedToGraph = true);
-    void removeVirtualLinks(int signatureId);
 
     rtabmap::Signature *_getSignature(int id) const;
-    int getNextId();
     void clear();
-
-    rtabmap::Signature *createSignature(const rtabmap::SensorData &data);
 
     //keypoint stuff
     void disableWordsRef(int signatureId);
     void cleanUnusedWords();
 
-    rtabmap::Transform computeGlobalVisualTransform(const rtabmap::Signature *oldSig, const rtabmap::Signature *newSig) const;
+protected:
+    rtabmap::DBDriver *_dbDriver;
 
 private:
-    std::vector<Database *> _databases;
+    // parameters
+    rtabmap::ParametersMap parameters_;
+    bool _badSignaturesIgnored;
+
+    std::map<int, rtabmap::Transform> _optimizedPoses;
+    std::map<int, rtabmap::Signature *> _signatures; // TODO : check if a signature is already added? although it is not supposed to occur...
+
+    //Keypoint stuff
+    rtabmap::VWDictionary *_vwd;
+    rtabmap::Feature2D *_feature2D;
 };
