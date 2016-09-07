2b12aa478c1e79533cf33e22800e7a735a50f53c
Kaifei Chen <kaifei@berkeley.edu>
2016-09-07 01:27:22 +0000

    CellMateServer compiles and works

--- server/distributed/src/service/CellMateServer.cpp
+++ server/distributed/src/service/CellMateServer.cpp
@@ -1,14 +1,28 @@
-#include "algo/Feature.h"
-#include "service/CellMate.grpc.pb.h"
+#include "service/CellMateServer.h"
+#include "CellMate.grpc.pb.h"
+#include "adapter/RTABMapDBAdapter.h"
+#include "data/CameraModel.h"
+#include "data/LabelsSimple.h"
+#include "data/Session.h"
+#include "data/SignaturesSimple.h"
+#include "data/WordsKdTree.h"
+#include "util/Time.h"
+#include <QDebug>
 #include <grpc++/grpc++.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
 
 // Take in the "service" instance (in this case representing an asynchronous
 // server) and the completion queue "cq" used for asynchronous communication
 // with the gRPC runtime.
-CallData::CallData(CellMate::AsyncService *service,
-                   grpc::ServerCompletionQueue *cq, Feature &feature)
+CallData::CallData(proto::CellMate::AsyncService *service,
+                   grpc::ServerCompletionQueue *cq, Feature &feature,
+                   WordSearch &wordSearch, SignatureSearch &signatureSearch,
+                   Perspective &perspective, Visibility &visibility)
     : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE),
-      _feature(feature) {
+      _feature(feature), _wordSearch(wordSearch),
+      _signatureSearch(signatureSearch), _perspective(perspective),
+      _visibility(visibility) {
   // Invoke the serving logic right away.
   proceed();
 }
@@ -23,50 +37,73 @@ void CallData::proceed() {
     // the tag uniquely identifying the request (so that different CallData
     // instances can serve different requests concurrently), in this case
     // the memory address of this CallData instance.
-    service_->RequestDetect(&ctx_, &request_, &responder_, cq_, cq_, this);
+    service_->RequestDetect(&ctx_, &query_, &responder_, cq_, cq_, this);
   } else if (status_ == PROCESS) {
     // Spawn a new CallData instance to serve new clients while we process
     // the one for this CallData. The instance will deallocate itself as
     // part of its FINISH state.
-    new CallData(service_, cq_);
+    new CallData(service_, cq_, _feature, _wordSearch, _signatureSearch,
+                 _perspective, _visibility);
 
     // The actual processing.
     const bool copyData = false;
-    std::vector<char> data(request_.image().begin(), request_.image().end());
-    image = imdecode(cv::Mat(data, copyData), cv::IMREAD_GRAYSCALE);
+    std::vector<char> data(query_.image().begin(), query_.image().end());
+    cv::Mat image = imdecode(cv::Mat(data, copyData), cv::IMREAD_GRAYSCALE);
 
-    double fx = request_.cameramodel()->fx();
-    double fy = request_.cameramodel()->fy();
-    double cx = request_.cameramodel()->cx();
-    double cy = request_.cameramodel()->cy();
+    double fx = query_.cameramodel().fx();
+    double fy = query_.cameramodel().fy();
+    double cx = query_.cameramodel().cx();
+    double cy = query_.cameramodel().cy();
     int width = image.cols;
     int height = image.rows;
-    camera = CameraModel("", fx, fy, cx, cy, cv::Size(width, height));
+    CameraModel camera("", fx, fy, cx, cy, cv::Size(width, height));
 
     Session session;
-    session.id = request_.session()->id;
-    session.type = request_.session()->type;
-    session.overallStart = request_.session()->overallStart;
-    session.overallEnd = request_.session()->overallEnd;
-    session.featuresStart = request_.session()->featureStart;
-    session.featuresEnd = request_.session()->featureEnd;
-    session.wordsStart = request_.session()->wordsStart;
-    session.wordsEnd = request_.session()->wordsEnd;
-    session.signaturesStart = request_.session()->signaturesStart;
-    session.signaturesEnd = request_.session()->signaturesEnd;
-    session.perspectiveStart = request_.session()->perspectiveStart;
-    session.perspectiveEnd = request_.session()->perspectiveEnd;
+    session.id = query_.session().id();
+    if (query_.session().type() == proto::Session::HTTP_POST) {
+      session.type = HTTP_POST;
+    } else if (query_.session().type() == proto::Session::BOSSWAVE) {
+      session.type = BOSSWAVE;
+    }
+    session.overallStart = query_.session().overallstart();
+    session.overallEnd = query_.session().overallend();
+    session.featuresStart = query_.session().featuresstart();
+    session.featuresEnd = query_.session().featuresend();
+    session.wordsStart = query_.session().wordsstart();
+    session.wordsEnd = query_.session().wordsend();
+    session.signaturesStart = query_.session().signaturesstart();
+    session.signaturesEnd = query_.session().signaturesend();
+    session.perspectiveStart = query_.session().perspectivestart();
+    session.perspectiveEnd = query_.session().perspectiveend();
 
-    std::unique_ptr<cv::Mat> image(new cv::Mat());
-    std::unique_ptr<CameraModel> camera(new CameraModel());
+    std::vector<cv::KeyPoint> keyPoints;
+    cv::Mat descriptors;
+    session.featuresStart = getTime();
+    _feature.extract(image, keyPoints, descriptors);
+    session.featuresEnd = getTime();
 
-    _feature.extract(image, camera, session, *image, *camera);
+    session.wordsStart = getTime();
+    std::vector<int> wordIds = _wordSearch.search(descriptors);
+    session.wordsEnd = getTime();
+
+    session.signaturesStart = getTime();
+    std::vector<int> signatureIds = _signatureSearch.search(wordIds);
+    session.signaturesEnd = getTime();
+
+    int dbId;
+    Transform pose;
+    session.perspectiveStart = getTime();
+    _perspective.localize(wordIds, keyPoints, camera, signatureIds.at(0), dbId,
+                          pose);
+    session.perspectiveEnd = getTime();
+
+    std::vector<std::string> names = _visibility.process(dbId, camera, pose);
 
     // And we are done! Let the gRPC runtime know we've finished, using the
     // memory address of this instance as the uniquely identifying tag for
     // the event.
     status_ = FINISH;
-    responder_.Finish(reply_, Status::OK, this);
+    responder_.Finish(reply_, grpc::Status::OK, this);
   } else {
     GPR_ASSERT(status_ == FINISH);
     // Once in the FINISH state, deallocate ourselves (CallData).
@@ -74,10 +111,30 @@ void CallData::proceed() {
   }
 }
 
-~CellMateServer::CellMateServer() {
+CellMateServer::~CellMateServer() {
   server_->Shutdown();
   // Always shutdown the completion queue after the server.
   cq_->Shutdown();
+}
+
+bool CellMateServer::init(std::vector<std::string> dbfiles) {
+  std::unique_ptr<WordsKdTree> words(new WordsKdTree());
+  std::shared_ptr<SignaturesSimple> signatures(new SignaturesSimple());
+  std::unique_ptr<LabelsSimple> labels(new LabelsSimple());
+
+  std::cout << "Reading data" << std::endl;
+  if (!RTABMapDBAdapter::readData(dbfiles, *words, *signatures, *labels)) {
+    qCritical() << "Reading data failed";
+    return false;
+  }
+
+  _feature.reset(new Feature());
+  _wordSearch.reset(new WordSearch(std::move(words)));
+  _signatureSearch.reset(new SignatureSearch(signatures));
+  _perspective.reset(new Perspective(signatures));
+  _visibility.reset(new Visibility(std::move(labels)));
+
+  return true;
 }
 
 // There is no shutdown handling in this code.
@@ -104,7 +161,8 @@ void CellMateServer::run() {
 // This can be run in multiple threads if needed.
 void CellMateServer::handleRpcs() {
   // Spawn a new CallData instance to serve new clients.
-  new CallData(&service_, cq_.get(), _feature);
+  new CallData(&service_, cq_.get(), *_feature, *_wordSearch, *_signatureSearch,
+               *_perspective, *_visibility);
   void *tag; // uniquely identifies a request.
   bool ok;
   while (true) {
--- server/distributed/src/service/CellMateServer.h
+++ server/distributed/src/service/CellMateServer.h
@@ -1,4 +1,9 @@
-#include "service/CellMate.grpc.pb.h"
+#include "CellMate.grpc.pb.h"
+#include "algo/Feature.h"
+#include "algo/Perspective.h"
+#include "algo/SignatureSearch.h"
+#include "algo/Visibility.h"
+#include "algo/WordSearch.h"
 #include <grpc++/grpc++.h>
 
 // Class encompasing the state and logic needed to serve a request.
@@ -7,15 +12,17 @@ public:
   // Take in the "service" instance (in this case representing an asynchronous
   // server) and the completion queue "cq" used for asynchronous communication
   // with the gRPC runtime.
-  CallData(CellMate::AsyncService *service, grpc::ServerCompletionQueue *cq,
-           Feature &feature);
+  CallData(proto::CellMate::AsyncService *service,
+           grpc::ServerCompletionQueue *cq, Feature &feature,
+           WordSearch &wordSearch, SignatureSearch &signatureSearch,
+           Perspective &perspective, Visibility &visibility);
 
   void proceed();
 
 private:
   // The means of communication with the gRPC runtime for an asynchronous
   // server.
-  CellMate::AsyncService *service_;
+  proto::CellMate::AsyncService *service_;
   // The producer-consumer queue where for asynchronous server notifications.
   grpc::ServerCompletionQueue *cq_;
   // Context for the rpc, allowing to tweak aspects of it such as the use
@@ -24,24 +31,29 @@ private:
   grpc::ServerContext ctx_;
 
   // What we get from the client.
-  Query query_;
+  proto::Query query_;
   // What we send back to the client.
-  Empty reply_;
+  proto::Empty reply_;
 
   // The means to get back to the client.
-  grpc::ServerAsyncResponseWriter<Empty> responder_;
+  grpc::ServerAsyncResponseWriter<proto::Empty> responder_;
 
   // Let's implement a tiny state machine with the following states.
   enum CallStatus { CREATE, PROCESS, FINISH };
   CallStatus status_; // The current serving state.
 
   Feature &_feature;
+  WordSearch &_wordSearch;
+  SignatureSearch &_signatureSearch;
+  Perspective &_perspective;
+  Visibility &_visibility;
 };
 
 class CellMateServer final {
 public:
   ~CellMateServer();
 
+  bool init(std::vector<std::string> dbfiles);
   // There is no shutdown handling in this code.
   void run();
 
@@ -49,8 +61,12 @@ private:
   // This can be run in multiple threads if needed.
   void handleRpcs();
 
-  std::unique_ptr<ServerCompletionQueue> cq_;
-  CellMate::AsyncService service_;
-  std::unique_ptr<Server> server_;
-  Feature _feature;
+  std::unique_ptr<grpc::ServerCompletionQueue> cq_;
+  proto::CellMate::AsyncService service_;
+  std::unique_ptr<grpc::Server> server_;
+  std::unique_ptr<Feature> _feature;
+  std::unique_ptr<WordSearch> _wordSearch;
+  std::unique_ptr<SignatureSearch> _signatureSearch;
+  std::unique_ptr<Perspective> _perspective;
+  std::unique_ptr<Visibility> _visibility;
 };
