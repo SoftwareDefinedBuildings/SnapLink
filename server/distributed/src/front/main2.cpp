8ebb1e1cbd5ee1183cda0f507bd5a8b190424642
TongLi1992 <sasrwas@berkeley.edu>
2016-11-23 00:05:20 +0000

    copied the httpserver to bwserver, exactly same code but different name, and different output executable called bwfront, working fine, next step is to gradually change it to bwserver

--- server/distributed/src/front/main2.cpp
+++ server/distributed/src/front/main2.cpp
@@ -1,9 +1,9 @@
 #include "BWServer.h"
-//#include <QDebug>
-#include <iostream>
+#include <QDebug>
+
 int main(int argc, char *argv[]) {
   std::cout <<"Initializing BW server" << std::endl;
-/*  BWServer bwServer;
+  BWServer bwServer;
   std::string featureServerAddr(argv[2]); // = "0.0.0.0:50051";
   if (!bwServer.init(featureServerAddr)) {
     qCritical() << "Starting HTTP Server failed";
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
 
   std::string bwServerAddr(argv[1]); // = "0.0.0.0:50056";
   bwServer.run(bwServerAddr);
-*/
+
   return 0;
 }
 
