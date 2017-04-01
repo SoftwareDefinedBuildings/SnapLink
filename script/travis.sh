ad3f878d803765e85844f2ce4dac240b8f0cce83
TongLi1992 <sasrwas@berkeley.edu>
2017-04-01 18:17:12 +0000

    Improved travis CI so that it can check whether cellmate functioning well

---
+++ script/travis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get install python-imaging
+apt-get install python-requests
+
+cmake ..
+make 
+
+wget -r --no-parent https://people.eecs.berkeley.edu/~kaifei/download/buildsys16/410_demo/
+DATA_PATH=people.eecs.berkeley.edu/~kaifei/download/buildsys16/410_demo/
+
+./cellmate run $DATA_PATH/db/*.db & sleep 30 ; python ../test/http_client.py $DATA_PATH/test/ | tail -1 >  tail.txt
+result=1
+while read line; do
+    for word in $line; do
+        if [ $word = tests ]
+        then 
+            result=0
+        fi
+    done
+done <tail.txt
+
+exit $result
