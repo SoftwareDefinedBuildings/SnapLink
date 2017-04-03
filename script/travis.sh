240234c2a9963b4596ed41e94d23c1f9af0abbe7
TongLi1992 <sasrwas@berkeley.edu>
2017-04-03 21:48:11 +0000

    changed travis to analyz the accuracy of Cellmate

--- script/travis.sh
+++ script/travis.sh
@@ -13,14 +13,24 @@ wget -r --no-parent https://people.eecs.berkeley.edu/~kaifei/download/buildsys16
 DATA_PATH=people.eecs.berkeley.edu/~kaifei/download/buildsys16/410_demo/
 
 ./cellmate run $DATA_PATH/db/*.db & sleep 30 ; python ../test/http_client.py $DATA_PATH/test/ | tail -1 >  tail.txt
-result=1
-while read line; do
-    for word in $line; do
-        if [ $word = tests ]
-        then 
-            result=0
-        fi
-    done
-done <tail.txt
 
-exit $result
+tail=$(head -n 1 tail.txt)
+read num1 num2 num3 <<<${tail//[^0-9]/ }
+accuracy=$(( num1 * 100 / num2))
+if [ $accuracy -lt 90 ]
+then 
+    exit 1
+fi
+
+exit 0
+#result=1
+#while read line; do
+#    for word in $line; do
+#        if [ $word = tests ]
+#        then 
+#            result=0
+#        fi
+#    done
+#done <tail.txt
+
+#exit $result
